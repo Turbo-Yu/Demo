8fefba2138d02e3a454f825a5aa2c7a99ddfb21b
TurboFlag <365738593@qq.com>
2016-01-13 16:39:38 +0800

    增加一个转换Json类，对于未标记[DataContract]、[DataMember]实行转换

---
+++ DemoCore/Converter/ConvertJson/JsonHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NLog;
+
+namespace Demo.Core.Converter.ConvertJson
+{
+    public class JsonHelper
+    {
+        static readonly JsonPropertiesResolver JsonPropertiesResolver = new JsonPropertiesResolver();
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        private static readonly JsonSerializerSettings _jsonSerializerSettings = new JsonSerializerSettings { ContractResolver = JsonPropertiesResolver };
+
+        public static void Info<T>(T obj)
+        {
+            var jsonStr = JsonConvert.SerializeObject(obj, Formatting.Indented, _jsonSerializerSettings);
+            _logger.Info($"\r\n{(jsonStr)}");
+        }
+    }
+}
---
+++ DemoCore/Converter/ConvertJson/JsonPropertiesResolver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Demo.Core.Converter.ConvertJson
+{
+    /// <summary>
+    /// 未标记 DataContract,DataMember 类解析成JsonString形式
+    /// </summary>
+    public class JsonPropertiesResolver : DefaultContractResolver
+    {
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var property = base.CreateProperty(member, memberSerialization);
+            property.Ignored = false;
+            return property;
+        }
+
+        protected override List<MemberInfo> GetSerializableMembers(Type objecType)
+        {
+            return objecType.GetProperties().Where(x => x.GetIndexParameters().Length == 0).Cast<MemberInfo>().ToList();
+        }
+    }
+}
